96dfc6d58fdf8c9adddaf19306f49bb5c538b424
root <root@172-233-64-184.ip.linodeusercontent.com>
2025-09-29 17:28:53 +0900

    fix: lint 대상 범위 src로 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,13 @@
-// frontend/.eslintrc.cjs
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false,
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-  ],
-  plugins: ['vue'],
+  env: { node: true, es2021: true },
+  parserOptions: { ecmaVersion: 2021, sourceType: 'commonjs' },
+  extends: ['eslint:recommended'],
   rules: {
     semi: ['error', 'always'],
     quotes: ['error', 'single', { avoidEscape: true }],
-    'no-unused-vars': ['warn'],
+    'no-unused-vars': 'warn',
     'no-console': 'off',
-    'vue/no-unused-components': 'warn',
   },
-  ignorePatterns: ['dist/', 'node_modules/', 'coverage/'],
+  ignorePatterns: ['frontend/**', 'node_modules/', 'dist/', 'coverage/'],
 };
