ec013a38a78a764057c57646ea2eb3b900f0df53
root <root@172-233-64-184.ip.linodeusercontent.com>
2025-09-29 17:21:14 +0900

    chore: @eslint/js 제거

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,29 @@
+// frontend/.eslintrc.cjs
 module.exports = {
   root: true,
   env: {
     browser: true,
     node: true,
-    es2020: true,
+    es2021: true,
   },
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
     requireConfigFile: false,
-    ecmaVersion: 2020,
-    sourceType: "module"
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended"
+    'plugin:vue/essential',
+    'eslint:recommended',
   ],
-  plugins: ["vue"],
+  plugins: ['vue'],
   rules: {
-    "semi": ["error", "always"],
-    "quotes": ["error", "single", { "avoidEscape": true }],
-    "no-unused-vars": ["warn"],
-    "no-console": ["off"],
-    "vue/no-unused-components": "warn"
-  }
+    semi: ['error', 'always'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-unused-vars': ['warn'],
+    'no-console': 'off',
+    'vue/no-unused-components': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/'],
 };
